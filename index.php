26913b97714fded7818aa082894e1df3de6c8431
Doszhan Omralin <ffacool5@gmail.com>
2023-02-07 15:16:37 +0600

    My edit;

--- index.php
+++ index.php
@@ -5,18 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RusTechnica - сайт о техниках</title>
-    <link rel="stylesheet" и type="text/css" href = "styles/main.css">
+    <link rel="stylesheet" и type="text/css" href = "styles\main.css">
 </head>
 <body>
+
 <header>
     <?php
 require_once("header.php");
 ?>
 </header>
 
+
 <p>Authors: Rustam, Doszhan, Timur, Ernar, Kirill!</p>
 
-<img src="images\sultan.png" alt="Sultan.png">
+
+
+
 <footer>
     <?php
     require_once("footer.php");
