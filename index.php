10ea29c3e26595d3c23db5d856f97e447888f483
Doszhan Omralin <ffacool5@gmail.com>
2023-02-07 14:39:34 +0600

    my edit

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RusTechnica - сайт о техниках</title>
+    <link rel="stylesheet" и type="text/css" href = "styles/main.css">
 </head>
 <body>
 <header>
