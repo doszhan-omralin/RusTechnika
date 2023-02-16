9c8cd38d07e6ac073d8b658fb9be56b1af4e988e
Doszhan Omralin <ffacool5@gmail.com>
2023-02-16 18:12:22 +0600

    edited

--- footer.php
+++ footer.php
@@ -1,3 +1,9 @@
-<div class="footer">
-    
+<div class="footer" style="height:70px;">
+    <div class="container">
+    <div class="row">
+        <div class="col">Column</div>
+        <div class="w-100"></div>
+        <div class="col">Column</div>   
+    </div>
+    </div>
 </div>
